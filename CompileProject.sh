a32cf4fbc7978eede2131dfc2c9bb75997f3deb8
ezensich <ezensich@gmail.com>
2016-09-08 09:55:28 -0300

    corrections in ast

--- CompileProject.sh
+++ CompileProject.sh
@@ -4,12 +4,12 @@
 echo "-----------------------------------------------------------------------"
 echo "COMPILING JFLEX.."
 
-java -jar lib/jflex-1.6.1.jar -d src src/compilers/Lexer.jflex
+java -jar lib/jflex-1.6.1.jar -d src/compilers src/compilers/Lexer.jflex
 
 echo "-----------------------------------------------------------------------"
 echo "COMPILING CUP.."
 
-java -jar lib/java-cup-11b.jar -destdir src src/compilers/Parser.cup 
+java -jar lib/java-cup-11b.jar -destdir src/compilers src/compilers/Parser.cup 
 
 echo "-----------------------------------------------------------------------"
 echo "COMPILING PROJECT.."
