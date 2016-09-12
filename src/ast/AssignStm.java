db607541ea01b851ed218e5f5e046e05fbdc0065
fmollea <fmollea@gmail.com>
2016-09-12 11:21:48 -0300

    class AssignStm modified, methods declared

--- src/ast/AssignStm.java
+++ src/ast/AssignStm.java
@@ -8,6 +8,38 @@ public class AssignStm extends Statement{
 	private Location location;
 	private AssignOpType operator;
 
+	// declared gets and sets
+	//sets
+	public void setExpression(Expression e){
+		this.expr = e;
+	}
+	
+	public void setLocation (Location l){
+		this.location = l;
+	}
+	
+	public void setAssignOpType (AssignOpType o){
+		this.operator = o;
+	}
+	
+	//gets 
+	public Location getLocation(){
+		return this.location;
+	}
+	
+	public Expression getExpression(){
+		return this.expr;
+	}
+	
+	public AssignOpType getAssignOpType(){
+		return this.operator;
+	}
+	
+	//method toString
+	public String toString() {
+		return location + " " + operator + " " + expr;
+	}
+	
 	
 	public <T> T accept(ProgramVisitor<T> v) {
 		return v.visit(this);
